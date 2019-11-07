deaf0a91d739d625e7875830df252988ffda54f5
Kevin Harvey <kcharvey@gmail.com>
2019-11-07 13:20:07 -0600

    Updates index to use template

--- telemat/views.py
+++ telemat/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h1>Hello TeleMAT!</h1>')
+    template_name = "index.html"
+    return render(request, template_name)
